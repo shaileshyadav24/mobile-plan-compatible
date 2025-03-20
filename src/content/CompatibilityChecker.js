927e235d4478e479c51165ce5ffa0ef10ff687ab
shaileshyadav24 <shaileshkyadav24@gmail.com>
2025-03-20 16:04:53 -0400

    Updated content

--- src/content/CompatibilityChecker.js
+++ src/content/CompatibilityChecker.js
@@ -1,5 +1,5 @@
 export const CompatibilityCheckerContent = {
-    title: 'US Mobile Compatibility Checker',
+    title: 'Mobile Compatibility Checker',
     description: "You can enter your phone model or IMEI number to check if it’s compatible with US Mobile.",
     errorMessaage: "Something went wrong...Please try again.",
 }
